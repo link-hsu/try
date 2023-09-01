74b5971258199422be4aef924b5b2ae9311b1183
Link Hsu <roccoisgood@gmail.com>
2023-09-01 21:17:41 +0800

    add sql file

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ app=Flask(__name__)
 def index():
 	return render_template("index.html")
 
-app.run(host="0.0.0.0", port=3000)
-# app.run(host="54.224.206.32", port=3000)
+# app.run(host="0.0.0.0", port=3000)
+app.run(host="54.224.206.32", port=3000)
 # app.run(port=3000)
