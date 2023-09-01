2c7080a6471f11c2a7a9168c0fda43567405c8fc
Link Hsu <roccoisgood@gmail.com>
2023-09-01 16:13:33 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import *
+
+app=Flask(__name__)
+
+# attractions route setting
+
+
+# Pages
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+# app.run(host="0.0.0.0", port=3000)
+# app.run(host="54.224.206.32", port=3000)
+app.run(port=3000)
